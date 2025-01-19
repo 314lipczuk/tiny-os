99f6385a177bbcf1630a89cd5b56668a04724fcf
314lipczuk <3.14lipczuk@gmail.com>
2025-01-19 17:26:14 +0100

    printing via calling sbi putchar from asm

--- src/main.zig
+++ src/main.zig
@@ -28,7 +28,41 @@ pub fn main() !void {
     const bss_len = @intFromPtr(bss_end) - @intFromPtr(bss);
     @memset(bss[0..bss_len], 0);
     for ("Hello world\n") |b| {
-        uart_buf_reg.* = b; // we write bytes one by one to UART fifo register (and quemu prints them)
+        //uart_buf_reg.* = b; // we write bytes one by one to UART fifo register (and quemu prints them)
+        puchar_sbi(b);
     }
     //syscon.* = 0x5555; // send powerdown; commented, cause qemu restarts image, making it an infinite loop of hello worlds
+}
+
+const sbi_ret = struct {
+    error_: isize,
+    value: usize,
+};
+// longs and ptrs are the size of integer register
+fn sbi_call(arg0: usize, arg1: usize, arg2: usize, arg3: usize, arg4: usize, arg5: usize, fid: usize, eid: usize) sbi_ret {
+    var a0: isize = undefined;
+    var a1: usize = undefined;
+    asm volatile (
+        \\ecall
+        : [a0] "={a0}" (a0),
+          [a1] "={a1}" (a1),
+        : [arg0] "{a0}" (arg0),
+          [arg1] "{a1}" (arg1),
+          [arg2] "{a2}" (arg2),
+          [arg3] "{a3}" (arg3),
+          [arg4] "{a4}" (arg4),
+          [arg5] "{a5}" (arg5),
+          [fid] "{a6}" (fid),
+          [eid] "{a7}" (eid),
+        : "memory"
+    );
+    return sbi_ret{ .error_ = a0, .value = a1 };
+}
+
+fn puchar_sbi(c: u8) void {
+    _ = sbi_call(c, 0, 0, 0, 0, 0, 0, 1);
+}
+
+fn putchar(c: u8) void {
+    uart_buf_reg.* = c;
 }
